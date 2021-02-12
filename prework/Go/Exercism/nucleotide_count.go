84787c1efc1d9fc4a6f91389c09e806b23ab6d61
yhabib <habibfernandez@gmail.com>
2021-02-12 19:04:42 +0100

    [prework](go) exercises from exercism

---
+++ prework/Go/Exercism/nucleotide_count.go
@@ -0,0 +1,28 @@
+package dna
+
+import "errors"
+
+// Histogram is a mapping from nucleotide to its count in given DNA.
+type Histogram map[rune]int
+
+// DNA is a list of nucleotides
+type DNA string
+
+// Counts generates a histogram of valid nucleotides in the given DNA.
+// Returns an error if d contains an invalid nucleotide.
+///
+// Counts is a method on the DNA type. A method is a function with a special receiver argument.
+// The receiver appears in its own argument list between the func keyword and the method name.
+// Here, the Counts method has a receiver of type DNA named d.
+func (d DNA) Counts() (Histogram, error) {
+	h := Histogram{'A': 0, 'C': 0, 'G': 0, 'T': 0}
+
+	for _, c := range d {
+		if _, ok := h[c]; !ok {
+			return h, errors.New("invalid nucleotide")
+		}
+		h[c]++
+	}
+
+	return h, nil
+}
