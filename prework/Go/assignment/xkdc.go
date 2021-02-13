3e2de25cb06dc7e52ca75783b0ba1f4f8a0bbb9d
yhabib <habibfernandez@gmail.com>
2021-02-13 18:23:01 +0100

    [prework](go) xkdc basic functionality

--- prework/Go/assignment/xkdc.go
+++ prework/Go/assignment/xkdc.go
@@ -8,83 +8,166 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
-const (
-	pathToComicCount = "https://xkcd.com/info.0.json"
-	pathToComic      = "https://xkcd.com/%d/info.0.json"
-	pathToIndex      = "./index.json"
-)
-
-// Comic represetns the XDCD structure
+//  ! Where to handle errors? here or at the fetch level?? What error do I throw here. Strategy for error handling
+// Can I have some multifield structure? Year, Month, Day       string `json:"year", "month", "day"`
+// Comic is the representation of xkdc Comic
 type Comic struct {
-	Num              int
-	Year, Month, Day string
-	Title            string
-	Transcript       string
-	Alt              string
-	Image            string
+	Num        int    `json:"num,omitempty"`
+	Year       string `json:"year,omitempty"`
+	Month      string `json:"month,omitempty"`
+	Day        string `json:"day,omitempty"`
+	Title      string `json:"title"`
+	Transcript string `json:"transcript"`
+	Alt        string `json:"alt,omitempty"`
+	Image      string `json:"image,omitempty"`
+	URL        string `json:"url"`
+}
+
+// StoredComic is the representation of a Comic in file
+type StoredComic struct {
+	URL        string `json: "url"`
+	Transcript string `json: "transcript"`
 }
 
 func main() {
-	createIndex()
+	fmt.Print("Enter pattern to search: ")
+	pattern, err := readInput()
+	if err != nil {
+		log.Printf("enter pattern fails: %v", err)
+	}
+
+	pathToFile, err := createIndex()
+	if err != nil {
+		log.Printf("create index fails: %v", err)
+	}
+
+	matches, err := searchForPattern(pattern, pathToFile)
+	if err != nil {
+		log.Printf("search for pattern fails: %v", err)
+	}
+	if len(matches) == 0 {
+		fmt.Println("No matches found")
+		os.Exit(1)
+	}
+	for i, match := range matches {
+		fmt.Printf("%d:\t%s \n\t%s\n", i+1, match.URL, match.Transcript)
+	}
 }
 
-func createIndex() bool {
+func readInput() (string, error) {
+	var input string
+	fmt.Scanln(&input)
+
+	return input, nil
+}
+
+func searchForPattern(pattern string, pathToFile string) ([]StoredComic, error) {
+	var _, comics []Comic
+	var matches []StoredComic
+	fmt.Printf("Starting search for provided pattern: %s\n", pattern)
+	jsonFile, err := os.Open(pathToFile)
+	if err != nil {
+		return matches, fmt.Errorf("opening file %s: %v", pathToFile, err)
+	}
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return matches, err
+	}
+
+	if err := json.Unmarshal(byteValue, &comics); err != nil {
+		return matches, err
+	}
+
+	for _, comic := range comics {
+		if strings.Contains(comic.Transcript, pattern) {
+			matches = append(matches, StoredComic{comic.URL, comic.Transcript})
+		}
+	}
+	return matches, nil
+}
+
+func createIndex() (string, error) {
+	pathToFile := "./index.json"
+	fmt.Println("Checking for existance of index ...")
+	if byte, err := ioutil.ReadFile(pathToFile); err != nil && !os.IsNotExist(err) {
+		return "", err
+	} else if len(byte) > 0 {
+		fmt.Println("   File already exists √")
+		return pathToFile, nil
+	}
+
+	fmt.Println("Generating index ...")
 	comics, err := getComics()
 	if err != nil {
-		//  ! Where to handle errors? here or at the fetch level?? What error do I throw here
-		// fmt.Fprintf(os.Stderr, "page cannot be fetched: %s\n", resp.Status)
-		return false
+		return "", fmt.Errorf("createIndex fails: %v", err)
 	}
-	fmt.Println(comics)
 
-	file, _ := json.MarshalIndent(comics, "", " ")
-	err = ioutil.WriteFile(pathToIndex, file, 0644)
+	file, err := json.MarshalIndent(comics, "", "  ")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error writing to file: %v\n", err)
-		return false
+		return "", fmt.Errorf("parsin %s to JSON: %v", pathToFile, err)
 	}
-	return true
+	if err := ioutil.WriteFile(pathToFile, file, 0644); err != nil {
+		return "", fmt.Errorf("writing to file %s: %v", pathToFile, err)
+	}
+	fmt.Println("   File generated √")
+	return pathToFile, nil
 }
 
 func getComics() ([]Comic, error) {
-	count, _ := getCountOfComics()
-	fmt.Println(count)
+	pathToComic := "https://xkcd.com/%d/info.0.json"
 	var comics []Comic
-	for i := 1; i < 5; i++ {
+	count, err := getCountOfComics()
+	if err != nil {
+		return comics, fmt.Errorf("getComics fails: %v", err)
+	}
+	for i := 1; i < count-2416; i++ {
 		var comic Comic
 		path := fmt.Sprintf(pathToComic, i)
-		fmt.Println(path)
 		comic, err := getComic(path)
 		if err != nil {
-			return comics, err
+			return comics, fmt.Errorf("getComics fails: %v", err)
 		}
 		comics = append(comics, comic)
 	}
 	return comics, nil
 }
 
+func getCountOfComics() (int, error) {
+	pathToComicCount := "https://xkcd.com/info.0.json"
+	comic, err := getComic(pathToComicCount)
+	if err != nil {
+		return 0, fmt.Errorf("getCountOfComics fails: %v", err)
+	}
+	return comic.Num, nil
+}
+
 func getComic(url string) (Comic, error) {
 	var comic Comic
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error fetching comic: %v\n", err)
-		return comic, err
+		return comic, fmt.Errorf("fetching %s: %v", url, err)
 	}
-
 	defer resp.Body.Close()
-	if err = json.NewDecoder(resp.Body).Decode(&comic); err != nil {
-		fmt.Fprintf(os.Stderr, "error decoding the body: %v\n", err)
-		return comic, err
+
+	if resp.StatusCode != http.StatusOK {
+		return comic, fmt.Errorf("can't get comic %s: %s", url, resp.Status)
 	}
 
+	if err = json.NewDecoder(resp.Body).Decode(&comic); err != nil {
+		return comic, err
+	}
+	comic.URL = url
 	return comic, nil
 }
 
-func getCountOfComics() (int, error) {
-	comic, _ := getComic(pathToComicCount)
-	return comic.Num, nil
-}
+// Some final comments:
+//  Best way of handling errors? Log them low in the chain, exit the program high in the chain
+//  Marshalling and unmarshalling too many times
