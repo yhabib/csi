729b5d47f1ee707ad17995526378da7a4bb362d9
yhabib <habibfernandez@gmail.com>
2021-03-11 19:37:29 +0100

    [prework](algos) Search a 2d matrix

---
+++ prework/algorithms/search-a-2d-matrix.go
@@ -0,0 +1,37 @@
+package search
+
+func searchMatrix(matrix [][]int, target int) bool {
+	return searchSubMatrix(matrix, target, 0, len(matrix))
+}
+
+func searchSubMatrix(matrix [][]int, target int, start int, end int) bool {
+	if start >= end {
+		return false
+	}
+
+	mid := (start + end) / 2
+	length := len(matrix[mid])
+
+	if target >= matrix[mid][0] && target <= matrix[mid][length-1] {
+		return binarySearch(matrix[mid], target, 0, length)
+	} else if target > matrix[mid][length-1] {
+		return searchSubMatrix(matrix, target, mid+1, end)
+	} else {
+		return searchSubMatrix(matrix, target, start, mid)
+	}
+}
+
+func binarySearch(matrix []int, target int, start int, end int) bool {
+	if start >= end {
+		return false
+	}
+	mid := (start + end) / 2
+
+	if target == matrix[mid] {
+		return true
+	} else if target > matrix[mid] {
+		return binarySearch(matrix, target, mid+1, end)
+	} else {
+		return binarySearch(matrix, target, start, mid)
+	}
+}
