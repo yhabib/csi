6f1c3628944d009847a242642009440ce1acfb91
yhabib <habibfernandez@gmail.com>
2021-03-18 05:31:19 +0100

    [prework](algos) Open lock second interation BFS

--- prework/algorithms/openLock.go
+++ prework/algorithms/openLock.go
@@ -1,16 +1,21 @@
 
 import "strings"
 
+// Notes:
+//  - Initially I had a map for visited and another one for dead_ends but they can be merged into one
+
 func openLock(deadends []string, target string) int {
 	startingPoint := "0000"
 	visited := make(map[string]bool)
 	queue := []string{startingPoint}
 	minDistance := 0
 
-	// To make it constant lookup
-	dead_ends := make(map[string]bool)
 	for _, d := range deadends {
-		dead_ends[d] = true
+		visited[d] = true
+	}
+
+	if visited[startingPoint] {
+		return -1
 	}
 
 	for len(queue) > 0 {
@@ -23,21 +28,16 @@ func openLock(deadends []string, target string) int {
 				return minDistance
 			}
 
-			if dead_ends[vertex] {
-				size--
-				continue
-			}
-
 			for i := 0; i < len(vertex); i++ {
 				n := (int(vertex[i]-'0') + 1) % 10
 				p := (int(vertex[i]-'0') - 1 + 10) % 10
 				next := vertex[:i] + strconv.Itoa(n) + vertex[i+1:]
 				prev := vertex[:i] + strconv.Itoa(p) + vertex[i+1:]
-				if !dead_ends[next] && !visited[next] {
+				if !visited[next] {
 					queue = append(queue, next)
 					visited[next] = true
 				}
-				if !dead_ends[prev] && !visited[prev] {
+				if !visited[prev] {
 					queue = append(queue, prev)
 					visited[prev] = true
 				}
