f02e439193c35c428a8fac3691c2d512f7353ca3
yhabib <habibfernandez@gmail.com>
2021-03-29 17:07:43 +0200

    Go solution to Exercism

---
+++ prework/golang/Exercism/clock.go
@@ -0,0 +1,42 @@
+package clock
+
+import "fmt"
+
+type Clock struct {
+	hour   int
+	minute int
+}
+
+func (c *Clock) recalculateTime() {
+	c.hour += c.minute / 60
+	c.minute %= 60
+	if c.minute < 0 {
+		c.minute += 60
+		c.hour--
+	}
+
+	c.hour %= 24
+	if c.hour < 0 {
+		c.hour += 24
+	}
+}
+
+func New(hour, minute int) Clock {
+	c := Clock{hour: hour, minute: minute}
+	c.recalculateTime()
+	return c
+}
+
+func (c Clock) String() string {
+	return fmt.Sprintf("%02d:%02d", c.hour, c.minute)
+}
+
+func (c Clock) Add(minutes int) Clock {
+	c.minute += minutes
+	c.recalculateTime()
+	return c
+}
+
+func (c Clock) Subtract(minutes int) Clock {
+	return c.Add(-1 * minutes)
+}
