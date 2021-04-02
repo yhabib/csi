22d19e4dbc7405ee64ab125e4f89ec5927e95e06
yhabib <habibfernandez@gmail.com>
2021-04-02 10:36:36 +0200

    Refactors register for a more efficient use of mem

--- computer-systems/assembly-2/fib.asm
+++ computer-systems/assembly-2/fib.asm
@@ -2,17 +2,17 @@ section .text
 global fib
 fib:
 	push rbx				;if proc uses rbx first thing is to save prev value	
-	mov rbx, rdi		;mov argument to save register so its kept per stack frame
-	mov rax, rbx
-	cmp rax, 1
+	mov ebx, edi		;mov argument to save register so its kept per stack frame
+	mov eax, ebx
+	cmp eax, 1
 	jle .end				;returns if less or equal to 1
-	lea	rdi, [rbx-2]
+	lea	edi, [ebx-2]
 	call fib
 	push rax				;local variable holding return value of fib(n-2)
-	lea	rdi, [rbx-1]
+	lea	edi, [ebx-1]
 	call fib
 	pop r9					;recovers value of fib(n-1) into random register
-	add rax, r9			;rax contains return of fib(n-1) and we add fib(n-2)
+	add eax, r9d		;rax contains return of fib(n-1) and we add fib(n-2)
 
 .end:
 	pop rbx					;pops callee-save register
