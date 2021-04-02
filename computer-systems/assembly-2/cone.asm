3267e663ea3b5cd52a5c38cc2c01fa5654975156
yhabib <habibfernandez@gmail.com>
2021-04-02 18:44:37 +0200

    cone exercise

--- computer-systems/assembly-2/cone.asm
+++ computer-systems/assembly-2/cone.asm
@@ -1,6 +1,16 @@
+; V=1/3*pi*r^2*h
 default rel
+section .data
+	PI dd 3.141592
+	three dd 3.0
 
 section .text
 global volume
 volume:
+	; ymm0 is radius
+	; ymm1 is height
+	mulss xmm0, xmm0
+	mulss xmm0, xmm1
+	mulss xmm0, [PI]
+	divss xmm0, [three]
  	ret
