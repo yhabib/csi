f09aa3ca5bc06b589d44dc0c77d97ab26a76728d
yhabib <habibfernandez@gmail.com>
2021-04-03 03:56:59 +0200

    Refactors fib by moving around the uses of ebx so it requires one less interaction with the stack

--- computer-systems/assembly-2/fib.asm
+++ computer-systems/assembly-2/fib.asm
@@ -1,19 +1,18 @@
 section .text
 global fib
 fib:
-	push rbx				;if proc uses rbx first thing is to save prev value	
-	mov ebx, edi		;mov argument to save register so its kept per stack frame
-	mov eax, ebx
+	mov eax, edi
 	cmp eax, 1
 	jle .end				;returns if less or equal to 1
-	lea	edi, [ebx-2]
+	push rbx				;if proc uses rbx first thing is to save prev value	
+	mov ebx, edi		;mov argument to save register so its kept per stack frame
+	lea	edi, [ebx-2];using lea to calculate the value but not access the memory as it would have happened with mov
 	call fib
-	push rax				;local variable holding return value of fib(n-2)
 	lea	edi, [ebx-1]
+	mov ebx, eax;by moving this instruction after the calculation of ebx-1 we don't need to keep its value so it can be reused
 	call fib
-	pop r9					;recovers value of fib(n-1) into random register
-	add eax, r9d		;rax contains return of fib(n-1) and we add fib(n-2)
+	add eax, ebx		;rax contains return of fib(n-1) and we add fib(n-2)
+	pop rbx					;pops callee-save register
 
 .end:
-	pop rbx					;pops callee-save register
 	ret
