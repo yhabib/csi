40de8f9680af8a6ceea862e93d3180bf3ef0925a
yhabib <habibfernandez@gmail.com>
2021-05-05 03:11:21 +0200

    [advance-programming](concurrency) solution to debug problem 1

--- advanced-programming/concurrency/example-1.go
+++ advanced-programming/concurrency/example-1.go
@@ -7,10 +7,13 @@ import (
 
 func main() {
 	for i := 0; i < 10; i++ {
-		go func() {
+		go func(i int) {
 			fmt.Printf("launched goroutine %d\n", i)
-		}()
+		}(i)
 	}
 	// Wait for goroutines to finish
 	time.Sleep(time.Second)
 }
+
+// Problem: i it is not goroutine safe by usig it as a closure -> it should be passed as argument
+// Solution: Create a copy of i for each iteration
