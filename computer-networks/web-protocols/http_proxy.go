c42a84d8e1401cbf220120b33ca542e1674a479c
yhabib <habibfernandez@gmail.com>
2021-06-14 06:55:15 +0200

    [computer-networks] http-proxy echoes back

--- computer-networks/web-protocols/http_proxy.go
+++ computer-networks/web-protocols/http_proxy.go
@@ -6,14 +6,19 @@ import (
 )
 
 func main() {
-	data := make([]byte, 1024)
-
 	s := socket.New()
+	defer s.Close()
+
 	s.Bind(8080, [4]byte{0, 0, 0, 0})
 	s.Listen(2048)
 	s.Accept()
 	for {
-		size := s.Read(data)
-		fmt.Printf("Size: %d\nData: %s", size, data)
+		data := make([]byte, 1024)
+		size := s.Receive(data)
+		fmt.Printf("%s\n", data)
+		if size == 0 {
+			break
+		}
+		s.Send(data)
 	}
 }
--- computer-networks/web-protocols/socket/socket.go
+++ computer-networks/web-protocols/socket/socket.go
@@ -1,7 +1,6 @@
 package socket
 
 import (
-	"fmt"
 	"syscall"
 )
 
@@ -14,15 +13,16 @@ func check(err error) {
 }
 
 type Socket struct {
-	fd  int
-	nfd int
+	fd       int
+	nfd      int
+	sockAddr syscall.Sockaddr
 }
 
 func New() Socket {
 	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
 	check(err)
 
-	return Socket{fd, 0}
+	return Socket{fd, 0, nil}
 }
 
 func (s *Socket) Bind(port int, addr [4]byte) {
@@ -37,14 +37,23 @@ func (s *Socket) Listen(backlog int) {
 }
 
 func (s *Socket) Accept() {
-	nfd, _, err := syscall.Accept(s.fd)
+	nfd, sockAddr, err := syscall.Accept(s.fd)
 	check(err)
 	s.nfd = nfd
+	s.sockAddr = sockAddr
 }
 
-func (s *Socket) Read(buffer []byte) (size int) {
-	fmt.Println(s)
+func (s *Socket) Receive(buffer []byte) (size int) {
 	size, _, err := syscall.Recvfrom(s.nfd, buffer, 0)
 	check(err)
 	return size
+}
+
+func (s *Socket) Send(buffer []byte) {
+	syscall.Sendto(s.nfd, buffer, 0, s.sockAddr)
+}
+
+func (s *Socket) Close() {
+	syscall.Close(s.fd)
+	syscall.Close(s.nfd)
 }
