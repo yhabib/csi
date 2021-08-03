4909f9e2a69b9579022e6ed5635560cccb59e1cf
yhabib <habibfernandez@gmail.com>
2021-08-03 21:58:01 +0200

    [data-structures] Roring bitmap: union and intersect

--- data-structures-for-storage-and-retrieval/roaring-bitmaps/readme.md
+++ data-structures-for-storage-and-retrieval/roaring-bitmaps/readme.md
@@ -1,3 +1,12 @@
 # Roaring Bitmaps
 
 ## Bitmap
+
+A bitmap is a mapping from one system such as integers to bits. It is also known as bitmap index or a bit array.
+
+The memory is divided into units for bitmap. These units may range from a few bytes to several kilobytes. Each memory unit is associated with a bit in the bitmap. If the unit is occupied, the bit is 1 and if it is empty, the bit is zero.
+
+### API
+
+* set(uint32 x): It sets the "nth" bit to true, where nth is the provided input. Eg: x = 32 -> it will set the bit 32 to true
+* get(uint32 x): It checks the presence of the value in the structure. 
--- data-structures-for-storage-and-retrieval/roaring-bitmaps/uncompressed.go
+++ data-structures-for-storage-and-retrieval/roaring-bitmaps/uncompressed.go
@@ -12,6 +12,9 @@ func newUncompressedBitmap() *uncompressedBitmap {
 
 func (b *uncompressedBitmap) Get(x uint32) bool {
 	idx := x / wordSize
+	if idx >= uint32(len(b.data)) {
+		return false
+	}
 	offset := x % wordSize
 	return b.data[idx]&(1<<offset) != 0
 }
@@ -26,15 +29,32 @@ func (b *uncompressedBitmap) Set(x uint32) {
 	b.data[idx] |= (1 << offset)
 }
 
+// Go through both arrays and make usion of each bit
 func (b *uncompressedBitmap) Union(other *uncompressedBitmap) *uncompressedBitmap {
+	if len(other.data) > len(b.data) {
+		return other.Union(b)
+	}
+
 	var data []uint64
+	data = append(data, b.data...)
+	for i := 0; i < len(other.data); i++ {
+		data[i] |= other.data[i]
+	}
+
 	return &uncompressedBitmap{
 		data: data,
 	}
 }
 
 func (b *uncompressedBitmap) Intersect(other *uncompressedBitmap) *uncompressedBitmap {
+	if len(other.data) < len(b.data) {
+		return other.Intersect(b)
+	}
 	var data []uint64
+	data = append(data, b.data...)
+	for i := 0; i < len(b.data); i++ {
+		data[i] &= other.data[i]
+	}
 	return &uncompressedBitmap{
 		data: data,
 	}
